40f4a969c5ab112cafb1e898d601ed7a3e74a756
FZsiga02 <fzsigmond02@gmail.com>
2022-09-22 08:47:21 +0200

    Licit()

--- src/hu/petrik/festmeny/festmenyek/Festmeny.java
+++ src/hu/petrik/festmeny/festmenyek/Festmeny.java
@@ -11,7 +11,7 @@ public class Festmeny {
     private LocalDateTime legutolsoLicitIdeje;
     private boolean elkelt;
 
-    public Festmeny(String cim, String festo, String stilus){
+    public Festmeny(String cim, String festo, String stilus) {
         this.cim = cim;
         this.festo = festo;
         this.stilus = stilus;
@@ -21,23 +21,23 @@ public class Festmeny {
         return festo;
     }
 
-    public String getStilus(){
+    public String getStilus() {
         return stilus;
     }
 
-    public int getLicitekSzama(){
+    public int getLicitekSzama() {
         return licitekSzama;
     }
 
-    public int getLegmagasabbLicit(){
+    public int getLegmagasabbLicit() {
         return legmagasabbLicit;
     }
 
-    public LocalDateTime getLegutolsoLicitIdeje(){
+    public LocalDateTime getLegutolsoLicitIdeje() {
         return legutolsoLicitIdeje;
     }
 
-    public boolean getElkelt(){
+    public boolean getElkelt() {
         return elkelt;
     }
 
@@ -45,7 +45,17 @@ public class Festmeny {
         this.elkelt = elkelt;
     }
 
-    public void licit(int mertek){
-
+    public void licit() {
+        if (elkelt) {
+            System.out.println("Hiba! A festmény már elkelt!");
+        } else if (licitekSzama == 0) {
+            legmagasabbLicit = 100;
+            licitekSzama++;
+            legutolsoLicitIdeje = LocalDateTime.now();
+        } else {
+            legmagasabbLicit += legmagasabbLicit * 0.1;
+            licitekSzama++;
+            legutolsoLicitIdeje = LocalDateTime.now();
+        }
     }
 }
