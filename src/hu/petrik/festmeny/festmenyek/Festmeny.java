cb7c32b3eaf7cd5154063a635f48b7842acc9140
FZsiga02 <fzsigmond02@gmail.com>
2022-09-22 08:38:10 +0200

    Festmeny class

--- src/hu/petrik/festmeny/festmenyek/Festmeny.java
+++ src/hu/petrik/festmeny/festmenyek/Festmeny.java
@@ -1,4 +1,51 @@
 package hu.petrik.festmeny.festmenyek;
 
+import java.time.LocalDateTime;
+
 public class Festmeny {
+    private String cim;
+    private String festo;
+    private String stilus;
+    private int licitekSzama;
+    private int legmagasabbLicit;
+    private LocalDateTime legutolsoLicitIdeje;
+    private boolean elkelt;
+
+    public Festmeny(String cim, String festo, String stilus){
+        this.cim = cim;
+        this.festo = festo;
+        this.stilus = stilus;
+    }
+
+    public String getFesto() {
+        return festo;
+    }
+
+    public String getStilus(){
+        return stilus;
+    }
+
+    public int getLicitekSzama(){
+        return licitekSzama;
+    }
+
+    public int getLegmagasabbLicit(){
+        return legmagasabbLicit;
+    }
+
+    public LocalDateTime getLegutolsoLicitIdeje(){
+        return legutolsoLicitIdeje;
+    }
+
+    public boolean getElkelt(){
+        return elkelt;
+    }
+
+    public void setElkelt(boolean elkelt) {
+        this.elkelt = elkelt;
+    }
+
+    public void licit(int mertek){
+
+    }
 }
--- src/hu/petrik/festmeny/festmenyek/Main.java
+++ src/hu/petrik/festmeny/festmenyek/Main.java
@@ -2,6 +2,6 @@ package hu.petrik.festmeny.festmenyek;
 
 public class Main {
     public static void main(String[] args) {
-
+        Festmeny f1 = new Festmeny("Mona Lisa", "Leonardo da Vinci", "Reneszánsz" );
     }
 }
